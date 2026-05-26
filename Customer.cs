56f1ab273eaad67cca5eddf4ec4295395108737c
lizhuolix <lizhuoli@hotmail.com>
2026-05-26 19:01:18 +1200

    update Customer class

--- Customer.cs
+++ Customer.cs
@@ -7,9 +7,26 @@ using System.Threading.Tasks;
 namespace SkeletonFinalApp
 {
     public class Customer
-    {
+    {   //Properties
 
+        public string Name { get; set; }
+        public string CustomerID { get; set; }
+        public string ContactInfo { get; set; }
 
+        // login credentials and customer information will be stored.
+
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+        //Constructor
+        public Customer(string name, string customerID, string contactInfo, string userName, string passWord)
+        {
+            Name = name;
+            CustomerID = customerID;
+            ContactInfo = contactInfo;
+            UserName = userName;
+            PassWord = passWord;
+        }
 
 
     }//End of Class Customer
