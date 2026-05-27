195c41e5dffde45a14f3fcd1d7ac77a048a4de6e
lizhuolix <lizhuoli@hotmail.com>
2026-05-27 14:39:28 +1200

    change set to private for security concern

--- Customer.cs
+++ Customer.cs
@@ -9,14 +9,14 @@ namespace SkeletonFinalApp
     public class Customer
     {   //Properties
 
-        public string Name { get; set; }
-        public string CustomerID { get; set; }
-        public string ContactInfo { get; set; }
+        public string Name { get; private set; }
+        public string CustomerID { get; private set; }
+        public string ContactInfo { get; private set; }
 
         // login credentials and customer information will be stored.
 
-        public string UserName { get; set; }
-        public string PassWord { get; set; }
+        public string UserName { get; private set; }
+        public string PassWord { get; private set; }
 
         //Constructor
         public Customer(string name, string customerID, string contactInfo, string userName, string passWord)
@@ -27,7 +27,9 @@ namespace SkeletonFinalApp
             UserName = userName;
             PassWord = passWord;
         }
-
+        //Methods for customer actions such as viewing account details, making transactions, etc. will be implemented here.
+       
+        
 
     }//End of Class Customer
 }//End of Namespace
