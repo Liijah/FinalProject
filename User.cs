12ecb64482fb1d5c3263a8ee39f90e94c2ab1190
lizhuolix <lizhuoli@hotmail.com>
2026-06-03 21:51:39 +1200

    create a base class User to hold common properties     and methods for both Customer and Admin

---
+++ User.cs
@@ -0,0 +1,37 @@
+﻿
+
+        #nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SkeletonFinalApp
+    {
+
+    //create a base class User to hold common properties
+    //and methods for both Customer and Admin
+    public class User
+        {
+            //Fields - shared between Customer and Admin
+            public string Name { get; protected set; }
+            public string UserName { get; protected set; }
+            protected string _password;
+
+            //Constructor
+            public User(string name, string userName, string password)
+            {
+                Name = name;
+                UserName = userName;
+                _password = password;
+            }
+
+            // Shared method - both Customer and Admin need to verify password
+            public bool VerifyPassword(string inputPassword)
+            {
+                return _password == inputPassword;
+            }//End of VerifyPassword method
+
+        }//End of Class User
+    }//End of Namespace
+
