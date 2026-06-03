e7d2d29ea6428bed97cb93aba36bada55ab4e222
lizhuolix <lizhuoli@hotmail.com>
2026-06-03 19:34:49 +1200

    Creating a boolean and a do while loop in main to keep the menu running until the user decides to exit

--- Program.cs
+++ Program.cs
@@ -6,50 +6,57 @@ namespace SkeletonFinalApp
     {
         static void Main(string[] args)
         {
+            BankingService bankingService = new BankingService();
+            //Creating a boolean and a do while loop to keep the menu running until the user decides to exit
+            bool isRunning = true;
 
-            //Creating login Menu for Final Project
-            Console.WriteLine("*********Welcome To The Banking App*********");
-            Console.WriteLine("********************************************");
-            Console.WriteLine();
 
-            //Displays Menu to user 
-            Console.WriteLine("Choose from the Menu ");
-            Console.WriteLine("1.  Admin Login: ");
-            Console.WriteLine("2.  Customer Login: ");
-            Console.WriteLine("3.  Register new Customer: ");
-            Console.WriteLine("99. Exit Application: ");
-            
-
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
+            do
             {
-                case 1:
-                    //Goes to Admin login requiring a username and password
-                    //Both need to be in asterisk form for privacy
-                    //Will be a Methods
-                    break;
-                case 2:
-                    //Same as Admin but a Customer version
-                    //Same with password and username criteria
-                    //Will be Methods aswell
-                    break;
-                case 3:
-                //Registering a new Customer
-                //Asking for username and password creation
-                //Methods as well
-                    bankingService.HandleRegistration(); // handle registration logic in the BankingService class
-                  
-                    break;
-                case 99:
-                    Console.WriteLine("Exiting Application ");
-                    Environment.Exit(0);
+                //Creating login Menu for Final Project
+                Console.WriteLine("*********Welcome To The Banking App*********");
+                Console.WriteLine("********************************************");
+                Console.WriteLine();
+
+                //Displays Menu to user 
+                Console.WriteLine("Choose from the Menu ");
+                Console.WriteLine("1.  Admin Login: ");
+                Console.WriteLine("2.  Customer Login: ");
+                Console.WriteLine("3.  Register new Customer: ");
+                Console.WriteLine("99. Exit Application: ");
+
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        //Goes to Admin login requiring a username and password
+                        //Both need to be in asterisk form for privacy
+                        //Will be a Methods
                         break;
-                default:
-                    Console.WriteLine("Invalid input. Attempts (Display how many attempts left) ");
-                    break;
+                    case 2:
+                        //Same as Admin but a Customer version
+                        //Same with password and username criteria
+                        //Will be Methods aswell
+                        break;
+                    case 3:
+                        //Registering a new Customer
+                        //Asking for username and password creation
+                        //Methods as well
+                        bankingService.HandleRegistration(); // handle registration logic in the BankingService class
 
-            }//End of Switch
+                        break;
+                    case 99:
+                        isRunning = false;
+                        Console.WriteLine("Exiting Application ");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Attempts (Display how many attempts left) ");
+                        break;
 
+                }//End of Switch
+            } while (isRunning);//end of Do While Loop
 
 
 
